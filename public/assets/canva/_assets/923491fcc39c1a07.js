6ea010eb18877e13950f4193c670d3ea16831963
Moondip Konwar <moondipkonwar9@gmail.com>
2025-07-08 12:01:41 +0530

    Canva

---
+++ public/assets/canva/_assets/923491fcc39c1a07.js
@@ -0,0 +1,8 @@
+(self["webpackChunk_canva_web"] = self["webpackChunk_canva_web"] || []).push([[78505],{
+
+/***/ 669534:
+function(_, __, __webpack_require__) {__webpack_require__.n_x = __webpack_require__.n;const __web_req__ = __webpack_require__;__web_req__(905716);self._5f74ec40302898c5a55451c9fbd04240 = self._5f74ec40302898c5a55451c9fbd04240 || {};(function(__c) {__c.Trb={config:{language:"en-IN",df:{yMMMd:"d MMM yyyy",yMd:"d/M/yyyy",yMMM:"MMM yyyy"},kf:"Jan Feb Mar Apr May Jun Jul Aug Sept Oct Nov Dec".split(" "),lf:"January February March April May June July August September October November December".split(" "),Of:["dd *[/-] *mm *[/-] *yy","dd *[/-] *mm *[/-] *yyyy","dd *[/-] *mm"]}};
+}).call(self, self._5f74ec40302898c5a55451c9fbd04240);}
+
+}])
+//# sourceMappingURL=sourcemaps/923491fcc39c1a07.js.map
---
+++ public/assets/canva/_assets/f2f95275dfe7a1c8.js
@@ -0,0 +1,8 @@
+(self["webpackChunk_canva_web"] = self["webpackChunk_canva_web"] || []).push([[46495],{
+
+/***/ 891548:
+function(_, __, __webpack_require__) {__webpack_require__.n_x = __webpack_require__.n;const __web_req__ = __webpack_require__;__web_req__(905716);self._5f74ec40302898c5a55451c9fbd04240 = self._5f74ec40302898c5a55451c9fbd04240 || {};(function(__c) {__c.DVb=function(a){const b={...a.props};a.ba.Mr(({Vh:c,cI:d})=>{c=a.rGb?{}:{sd:c()};a.ha.track(a.event,{...c,ym:d(),...b})})};__c.vH.prototype.PT=__c.ia(19,function(){});__c.GJ.prototype.PT=__c.ia(18,function(){this.ended||this.ML||(this.ML=!0,setTimeout(()=>{try{this.ended=!0,__c.$2a(this)}catch(a){this.G.Fb(a,{Ae:"Error ending span in next cycle",extra:new Map(__c.xJ(this))})}}))});
+}).call(self, self._5f74ec40302898c5a55451c9fbd04240);}
+
+}])
+//# sourceMappingURL=sourcemaps/f2f95275dfe7a1c8.js.map
